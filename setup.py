ca067ee48984559583e776497a9df3e134f50a6b
kennell <kevin@fileperms.org>
2016-02-27 21:00:21 +0100

    bump version to 0.15

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'schiene',
   packages = ['schiene'],
-  version = '0.14',
+  version = '0.15',
   license = 'MIT',
   description = 'schiene is a Python library for interacting with Bahn.de',
   author = 'Kevin Kennell',
