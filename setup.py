a222c97639cc7092068788366660f5322b403375
kennell <kevin@kennell.de>
2016-06-03 09:11:43 +0200

    update install_requires

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
   author = 'Kevin Kennell',
   author_email = 'kevin@kennell.de',
   install_requires=[
-        'requests>=2.7.0',
-        'beautifulsoup4>=4.4.0'
+        'requests>=2.10.0',
+        'beautifulsoup4>=4.4.1'
   ],
   url = 'https://github.com/kennell/schiene',
   keywords = ['bahn', 'api'],
