19c67a79557e61e694fe3a9d0ebb3f3d8cc079ae
Kevin Kennell <kennell@users.noreply.github.com>
2017-11-28 20:56:40 +0100

    bump to 0.19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'schiene',
   packages = ['schiene'],
-  version = '0.18',
+  version = '0.19',
   license = 'MIT',
   description = 'schiene is a Python library for interacting with Bahn.de',
   author = 'Kevin Kennell',
